c5ae52fac1421c0e64e0cb6642f4d5c3ba0f1491
aveswww <163146484+aveswww@users.noreply.github.com>
2024-03-27 10:35:17 +0100

    Update serwer.js

--- serwer.js
+++ serwer.js
@@ -2,8 +2,10 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function(req, odpowiedzSerwera){
-odpowiedzSerwera.writeHead(200, {'content-type': 'text/plain'});
-var html = fs.readFileSync('./cw1.js');
+odpowiedzSerwera.writeHead(200, {'content-type': 'text/html'});
+var html = fs.readFileSync('./index.html','utf-8');
+var header = "Witaj na stronie w Node";
+html = html.replace('{ Header }',header);
 odpowiedzSerwera.end(html);
 }).listen(3000);
 console.log('Serwer uruchomiony...');
